62480a9ca8adb54b25c7b5d444c02a627d1f3ab6
Jacob Grahn <contact@jacobgrahn.com>
2018-10-21 19:29:29 -0400

    work on saving images to s3

--- pr2-level-converter/src/stash-file.js
+++ pr2-level-converter/src/stash-file.js
@@ -1,5 +1,13 @@
-const stashFile = async (name, buffer) => {
+const AWS = require('aws-sdk') // eslint-disable-line import/no-extraneous-dependencies
+const s3 = new AWS.S3()
+const bucket = process.env.BUCKET || 'test-bucket'
 
+const stashFile = (key, buffer) => {
+  return s3.putObject({
+    Bucket: bucket,
+    Key: key,
+    Body: buffer
+  }).promise()
 }
 
 module.exports = stashFile
