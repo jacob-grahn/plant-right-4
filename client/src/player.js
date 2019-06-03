7792c57b295b1964bb8d3bb9a9b6442589d2bf3e
Jacob Grahn <contact@jacobgrahn.com>
2019-06-02 22:46:19 -0400

    make the physics more PR like

--- client/src/player.js
+++ client/src/player.js
@@ -1,4 +1,5 @@
 import { rotateVector } from './rotateVector'
+import { PlayerAttributes } from './player-attributes'
 import 'phaser'
 
 export class Player {
@@ -6,12 +7,14 @@ export class Player {
         this.sprite = scene.physics.add.sprite(x, y, 'dude')
         this.sprite.body.gravity = { x: 0, y: 1000 }
         this.sprite.body.maxSpeed = 1000
-        this.sprite.body.useDamping = true
-        this.sprite.body.setDrag(0.95, 0.95)
+        // this.sprite.body.useDamping = true
+        // this.sprite.body.setDrag(0.95, 0.95)
         this.sprite.body.setSize(25, 25)
         this.canRotate = true
         this.sprite.externalAcceleration = { x: 0, y: 0 }
+        this.attributes = new PlayerAttributes()
 
+        console.log(this.attributes.velX, this.attributes.velX, this.attributes.ease)
         this.onRotate()
 
         scene.anims.create({
@@ -39,21 +42,23 @@ export class Player {
         const sprite = this.sprite
         const body = sprite.body
         const accel = new Phaser.Math.Vector2(0, 0)
+        const rotatedVelocity = rotateVector({x: body.velocity.x, y: body.velocity.y}, -this.sprite.angle)
         
         if (cursors.left.isDown) {
-            accel.x = -800
+            accel.x = (-this.attributes.velX - rotatedVelocity.x) * this.attributes.ease
             sprite.anims.play('left', true)
         }
         else if (cursors.right.isDown) {
-            accel.x = 800
+            accel.x = (this.attributes.velX - rotatedVelocity.x) * this.attributes.ease
             sprite.anims.play('right', true)
         }
         else {
+            accel.x = (0 - rotatedVelocity.x) * this.attributes.ease
             sprite.anims.play('turn')
         }
 
         if (cursors.up.isDown && sprite.body.blocked[this.dir]) {
-            accel.y = -60000
+            accel.y = -this.attributes.velY
         }
 
         if (cursors.down.isDown) {
@@ -66,10 +71,12 @@ export class Player {
         }
 
         const rotatedAccel = rotateVector(accel, this.sprite.angle)
-        sprite.body.setAcceleration(
-            this.sprite.externalAcceleration.x + rotatedAccel.x,
-            this.sprite.externalAcceleration.y + rotatedAccel.y
-        )
+        body.velocity.x += this.sprite.externalAcceleration.x + rotatedAccel.x
+        body.velocity.y += this.sprite.externalAcceleration.y + rotatedAccel.y
+
+        // limit speed so we can't run through blocks
+        body.velocity.x = Phaser.Math.Clamp(body.velocity.x, -800, 800)
+        body.velocity.y = Phaser.Math.Clamp(body.velocity.y, -800, 800)
 
         this.sprite.externalAcceleration.x = 0
         this.sprite.externalAcceleration.y = 0
