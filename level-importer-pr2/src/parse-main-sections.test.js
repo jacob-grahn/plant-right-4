3bbda24d5a5687040aea10b98f196ba8aa7cc64b
Jacob Grahn <contact@jacobgrahn.com>
2019-06-06 21:46:39 -0400

    filling out unit tests

---
+++ level-importer-pr2/src/parse-main-sections.test.js
@@ -0,0 +1,30 @@
+const parseMainSections = require('./parse-main-sections')
+
+test('parse main section', () => {
+    const arr = [
+        'fileVersion',
+        'fadeColor',
+        'blocks',
+        'art1',
+        'art2',
+        'art3',
+        'art4',
+        'u1',
+        'u2',
+        'bg'
+    ]
+    const str = arr.join('`')
+
+    expect(parseMainSections(str)).toEqual({
+        fileVersion: 'fileVersion',
+        fadeColor: 'fadeColor',
+        blocks: ['blocks'],
+        art1: ['art1'],
+        art2: ['art2'],
+        art3: ['art3'],
+        art4: ['art4'],
+        u1: ['u1'],
+        u2: ['u2'],
+        bg: 'bg'
+    })
+})
