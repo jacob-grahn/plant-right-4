0f68f203376a0175577e613e774164d15532674e
bls1999 <bls1999@users.noreply.github.com>
2019-06-12 02:30:16 -0400

    rework tileEffects to include bumpHandlers
    I'm not sure if this is how you want to tackle this, but here's a start I suppose!
    
    Also adds .DS_Store (mac config file) to .gitignore.

--- client/src/tile-effects/index.js
+++ client/src/tile-effects/index.js
@@ -1,22 +1,65 @@
+/* for when these are added
+import { crumble } from './crumble'
+import { vanish } from './vanish'
+import { push } from './push'
+import { safetyNet } from './safetyNet'
+*/
+import { down } from './down'
+import { up } from './up'
 import { left } from './left'
 import { right } from './right'
-import { up } from './up'
-import { down } from './down'
 import { mine } from './mine'
 
+
+/* for when these are added
+import { item } from './item'
+import { finish } from './finish'
+import { rotateClockwise } from './rotateClockwise'
+import { rotateCounterclockwise } from './rotateCounterclockwise'
+import { infiniteItem } from './infiniteItem'
+import { happy } from './happy'
+import { sad } from './sad'
+import { heal } from './heal'
+import { time } from './time'
+*/
+import { brick } from './brick'
+
 const collideHandlers = {
-    up,
+    /*crumble,
+    vanish,
+    push,
+    safetyNet,*/
     down,
+    up,
     left,
     right,
     mine
 }
 
+const bumpHandlers = {
+    /*item,
+    finish,
+    rotateClockwise,
+    rotateCounterclockwise,
+    infiniteItem,
+    happy,
+    sad,
+    heal,
+    time,*/
+    brick
+}
+
 export function tileEffects (playerSprite, tile) {
+    let handlers = null
     if (tile.properties.collideHandlers) {
-        const collideHandlerNames = tile.properties.collideHandlers.split(',')
-        collideHandlerNames.forEach((handlerName) => {
-            const handler = collideHandlers[handlerName]
+        handlers = tile.properties.collideHandlers
+    } else if (tile.properties.bumpHandlers) {
+        handlers = tile.properties.bumpHandlers
+    }
+    if (handlers) {
+        const handlerNames = handlers.split(',')
+        handlerNames.forEach((handlerName) => {
+            const handler = handlers[handlerName]
             if (handler) {
                 handler(playerSprite, tile)
             }
