96ad18f79cacc71779d885962e588e13de477ed3
GambitAnimating <lumarigamesdev@gmail.com>
2019-06-20 08:34:26 -0400

    Added explosion animation and sfx when hit mine (#15)
    * Update README.md
    
    Just made it simpler for people to know what to do
    
    * Rotation Changes
    
    Made rotation more PR like, tweening it :) Feel free to make any changes you'd like
    
    * Added mine "exploding"
    
    Added mines to actually push player away like in the other PRs, this is just a start for this and can of course be changed to your liking but I hope its a good enough start! Has recovery also so that can be used for other hurt blocks later easily, if needed.
    
    * Implemented Spine, and (started particle effect)
    
    Particle Effect is very work in progress but implemented spine and made a character that looks like pr2s default guy. Has idle and running animation currently.
    
    * Changed depth in front of blocks
    
    * Jump Anim, Bump Handler, and more
    
    Fixed RotateVector, added jump anim to player, and also implemented bump handler
    
    * Added multiple parts and added shatter effect
    
    Started adding multiple parts, sorted out how part selection and coloring will work in 2 simple functions, and added shatter effect when bumping a brick.
    
    * Moved some things around and more
    
    Moved things around, and also fixed the hex colors. (Any hex color works now as before it wouldnt)
    
    * Started making ledge jumping
    
    Example: https://streamable.com/0r6o1 started making this, maybe not in the best way over the coming days ill try to rethink how to do it best but any help would be great if you've got any ideas!
    
    * Complies to javascript standards
    
    Fixed code to comply to javascript standards, and also fixed crouching so that you cant ledge jump now.
    
    * Added crouch and crouchwalk anim
    
    Just added the anims and set it up
    
    * Made crouch walk exaggerated
    
    * Explosion Animation and Sfx
    
    Added explosion animation and sfx when you destroy a mine, and tried to make code pretty aha.

---
+++ client/src/Explosion.js
@@ -0,0 +1,22 @@
+import { GetScene } from './main.js'
+
+export class Explosion {
+  static CreateExplosion (x, y) {
+  //  Create explosion animation, set scale and play
+    var explosion = GetScene().add.spine(x, y, 'Explosion', 'Explode', true)
+    explosion.setScale(0.2)
+
+    //  Play sound once and destroy after
+    GetScene().sound.play('explodesfx')
+
+    //  Create a Event Listener to know when to destroy self
+    explosion.state.addListener({
+      event: function (trackIndex, event) {
+        switch (event.data.name) {
+          case ('DeleteSelf') :
+            explosion.destroy()
+            break
+        }
+      } })
+  }
+}
